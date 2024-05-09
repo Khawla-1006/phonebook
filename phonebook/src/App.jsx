8bc72f022526227d66b81cb9e89dc3e9a85a0ed0
Khawla-1006 <khawla.ben.in@gmail.com>
2024-05-10 00:06:37 +0200

    Phonebook Step 1

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,10 +1,32 @@
 import { useState } from 'react'
 
+const App = () =>{
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
 
-function App() {
- 
+  const [newName, setNewName] = useState('')
+
+  return(
+    <div>
+      <h2>Phonebook</h2>
+
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+    </div>
+  )
+
 
 
 }
+
 
 export default App
