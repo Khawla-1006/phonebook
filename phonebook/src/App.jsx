7b5ac35260069d838a6e4e762fb15be07f8b091c
Khawla-1006 <khawla.ben.in@gmail.com>
2024-05-12 02:35:30 +0200

    phonebook step 3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -5,10 +5,12 @@ const App = () =>{
   const [persons, setPersons] = useState([
     { 
       name: 'Arto Hellas',
+      number: '123-456-7890',
       id : 1
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 
   const addName = (event) =>{
@@ -19,23 +21,32 @@ const App = () =>{
         if(persons[i].name.toLowerCase() === newName.toLowerCase()){
             alert(`${newName} is already added to phonebook`)
         }
+        if(persons[i].number === newNumber){
+          alert(`${newNumber} is already added to phonebook`)
+        }
     }
 
     const newNameObject = {
     name : newName,
+    number: newNumber,
     id: persons.length + 1
     }
     setPersons(persons.concat(newNameObject))
-    setNewName('add names!')
+    setNewName('add name')
+    setNewNumber('add number')
   }
 
   const handleNameChange = (event) =>{
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+
   const Person = ({person}) =>{
     return (
-      <li>{person.name}</li>
+      <li>{person.name} : {person.number}</li>
     )
   }
 
@@ -51,19 +62,27 @@ const App = () =>{
               onChange={handleNameChange}
           />
         </div>
+        <br></br>
+        <div>
+          number: <input 
+              value={newNumber}
+              onChange={handleNumberChange}
+          />
+        </div>
         <div>
           <button type='submit' onClick={addName}>add</button>
-        <ul>
+          <ul>
           {persons.map(person => 
             <Person person={person} key={person.id}/>
           )}
-        </ul>
+          </ul>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>
-debug : {newName}
-      </div>
+
+        <h2>Numbers</h2>
+        <div>
+           debug : {newName}
+        </div>
     </div>
   )
 
